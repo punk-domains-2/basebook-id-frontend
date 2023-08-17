30563d2fcd3ec5a94c7c5d4b637d9650a772437e
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2023-08-17 21:52:59 +0200

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Basebook ID frontend
+
+Frontend for .basebook domain service powered by the [Punk Domains protocol](https://punk.domains).
+
+## Quickstart
+
+```bash
+npm install
+npm run dev
+```
+
+## tokens.json
+
+Add the correct tokens (for the Send Tokens page).
---
+++ src/views/About.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="container">
+  <h1 class="text-center">About Basebook ID</h1>
+  
+  <div class="row mt-3">
+    <div class="col-md-8 offset-md-2">
+      <p>
+        Basebook ID is a Web3 Social username service where anybody can mint their very own web3 domain username with a .basebook extension.
+      </p>
+
+    </div>
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: "About"
+  }
+</script>
+
+<style scoped>
+p {
+  text-align: justify;
+  font-size: 1.1em;
+}
+
+h3 {
+  margin-top: 35px;
+}
+</style>
